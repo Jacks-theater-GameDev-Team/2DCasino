e51d76e4cb7442a866500d781e7de147e648c145
RealCaptainObvious <ma.boekema@st.hanze.nl>
2024-06-02 01:18:26 +0200

    Rolling + Collision + Directional Sprites

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D body;
+    [SerializeField] private Sprite[] spriteArray;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     [SerializeField] private float horizontal;
     [SerializeField] private float vertical;
@@ -20,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         roll = false;
         rollCooldown = false;
     }
@@ -43,6 +46,24 @@ public class PlayerMovement : MonoBehaviour
         if (!roll)
         {
             body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+            if (horizontal > 0)
+            {
+                spriteRenderer.sprite = spriteArray[3];
+            }
+            else if (horizontal < 0)
+            {
+                spriteRenderer.sprite = spriteArray[2];
+            }
+
+            if (vertical > 0)
+            {
+                spriteRenderer.sprite = spriteArray[0];
+            }
+            else if (vertical < 0)
+            {
+                spriteRenderer.sprite = spriteArray[1];
+            }
         }
     }
 

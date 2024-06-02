2e30cf8d60c0daac6e4ed6e79d49630533dc98d4
mienappels <elivholland@gmail.com>
2024-06-02 21:13:20 +0200

    Bro ben nu al vergeten hoe collision werkt

--- Assets/Scripts/blackjack.cs
+++ Assets/Scripts/blackjack.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class blackjack : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] private bool beingTouched;
+
+    private Rigidbody rigidbody;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+                    beingTouched = true;
+            print("Being touched");
+        if (collision.gameObject.tag == "Player")
+        {
+            beingTouched = true;
+            print("Player is touching blackjack");
+        }
     }
 }

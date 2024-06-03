9eb72094343bc4066495571df19cbf1f6504c18e
mienappels <elivholland@gmail.com>
2024-06-03 22:08:23 +0200

    BlackjackController Added

--- Assets/Scripts/BlackjackController.cs
+++ Assets/Scripts/BlackjackController.cs
@@ -14,18 +14,79 @@ public class BlackjackController : MonoBehaviour
     public TextMeshProUGUI dealerScore;
     public TextMeshProUGUI playerScore;
 
+    // private string[4][13] playingCards;
 
-    void Start()
-    {
-        dealerCard1 = GameObject.Find("WTD1C");
-        dealerCard2 = GameObject.Find("WTD2C");
-        playerCard1 = GameObject.Find("WTP1C");
-        playerCard2 = GameObject.Find("WTP2C");
-    }
+    private GameState currentState;
 
-    // Update is called once per frame
-    void Update()
+
+
+    private enum GameState
     {
-        
+        Start,
+        PlayerTurn,
+        DealerTurn,
+        End
+}
+
+    private string[][] Cards;
+
+
+void Start()
+{
+
+
+
+
+
+    currentState = GameState.Start;
+    dealerCard1 = GameObject.Find("WTD1C");
+    dealerCard2 = GameObject.Find("WTD2C");
+    playerCard1 = GameObject.Find("WTP1C");
+    playerCard2 = GameObject.Find("WTP2C");
+}
+
+// Update is called once per frame
+void Update()
+{
+
+    runStateMachine();
+
+}
+
+// Sprite dealCard(string cardName)
+// {
+    // return card.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Cards/" + cardName);
+// }
+
+void runStateMachine()
+{
+    switch (currentState)
+    {
+        case GameState.Start:
+
+
+
+
+
+            // playerCard1.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Cards/2C");
+            //     playerCard2.GetComponent<SpriteRenderer>().sprite = dealCard();
+            // currentState = GameState.PlayerTurn;
+            break;
+
+        case GameState.PlayerTurn:
+
+            currentState = GameState.DealerTurn;
+            break;
+
+        case GameState.DealerTurn:
+
+            currentState = GameState.End;
+            break;
+
+        case GameState.End:
+
+            currentState = GameState.Start;
+            break;
     }
+}
 }
